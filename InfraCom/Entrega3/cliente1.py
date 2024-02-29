8c6059bce8b0d8bdbceaf9a656963f9651627359
mms-11 <140762703+mms-11@users.noreply.github.com>
2024-02-29 15:17:51 -0300

    Changes to be committed:

--- InfraCom/Entrega3/cliente1.py
+++ InfraCom/Entrega3/cliente1.py
@@ -1,7 +1,7 @@
 import socket
 
 def main():
-    HOST = '127.0.0.1'  # Endereço IP do servidor
+    HOST = '192.168.100.100'  # Endereço IP do servidor
     PORT = 5000         # Porta do servidor
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
