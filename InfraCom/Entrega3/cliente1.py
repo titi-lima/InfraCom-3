9533e3440d61bff98870d23ba73e8e1402a0e259
mms-11 <140762703+mms-11@users.noreply.github.com>
2024-03-05 11:55:14 -0300

    novo modelo

--- InfraCom/Entrega3/cliente1.py
+++ InfraCom/Entrega3/cliente1.py
@@ -1,22 +1,20 @@
 import socket
-import classeCliente
-
-def main():
-    HOST = '192.168.100.100'  # Endereço IP do servidor
-    PORT = 5000         # Porta do servidor
-    cli1 = Cliente();
-    cli1.nome = 'suruagy'
-    cli1.settar(HOST,PORT)
 
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+HOST = '192.168.100.100'  # Endereço IP do servidor
+PORT = 5000         # Porta do servidor
 
-    while True:
-        message = input("Digite uma mensagem para enviar ao servidor: ")
-        client_socket.sendto(message.encode(), (HOST, PORT)) #enviar a mensagem para o servidor com base no seu end IP(host) e porta do dervidor
+client_soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #criando socket tcp da familia ipv6
+client_soquete.connect((HOST,PORT))#CONECTANDO COM SERVIDOR
 
-        response, server_address = client_socket.recvfrom(1024) #tipo um ack
-        print(f"Resposta do servidor: {response.decode()}")
+#criar dado para enviar
+dado = "primeiro dado"
+dado = str.enconde(dado); #cofificar em string
 
-if __name__ == "__main__":
-    main()
+#enviar pro servidor
+client_soquete.sendall(dado);
+#receber
+dadoRecebido = client_soquete.recv(1024)#1o24 butes
+print("MENSAGEM RECEBIDA", dadoRecebido.decode())
+
+
